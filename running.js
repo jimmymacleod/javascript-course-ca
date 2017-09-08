c4f6e89439ca787f5761b9ec07e574421c63ff02
James MacLeod <31019678+jimmymacleod@users.noreply.github.com>
2017-09-08 16:13:03 +1000

    Update running.js
    For this exercise I am having trouble getting this step correct;
    
    "For people who registered early or are over 18 (but not both), log a statement to the console telling them that they will race at 11:00 am. Include their raceNumber."
    
    I am getting two lines of the output instead of the desired one line.
    
    
    Also when I add the else statement, I then get three lines of output:
    936, your race will start at 9.30. 936, your race will start at 11.00 Return to registration.

--- running.js
+++ running.js
@@ -14,3 +14,13 @@ let runnerEarly = true;
 	if ((runnerAge < 18) && (runnerEarly = false)){
     console.log(raceNumber + ', your race will start at 12.30')
   } ;
+
+// For this exercise I am having trouble getting this step correct;
+
+// "For people who registered early or are over 18 (but not both), log a statement to the console telling them that they will race at 11:00 am. Include their raceNumber."
+
+// I am getting two lines of the output instead of the desired one line.
+
+
+// Also when I add the else statement, I then get three lines of output:
+// 936, your race will start at 9.30. 936, your race will start at 11.00 Return to registration.
