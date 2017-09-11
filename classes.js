5c34e5bae179532dcfe9e96740138f8461537b09
Jimmy MacLeod <31019678+jimmymacleod@users.noreply.github.com>
2017-09-11 17:06:05 +1000

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,1 +1,36 @@
 
+// Inside of the constructor() method, we use the this keyword. 
+// In the context of a class, this refers to an instance of that class. 
+// In the Dog class, we use this to set the value of the Dog instance's name property to the name argument
+
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get department() {
+    return this._department;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+//use any name for the const variable?
+
+//?The syntax for calling methods and getters on an instance is the same as calling them on an object — append the instance with a period, then the property or method name. For methods, you must also include opening and closing parentheses.
