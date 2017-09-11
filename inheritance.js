c281a93a342332ef406fe973e6defdcefc72a683
Jimmy MacLeod <31019678+jimmymacleod@users.noreply.github.com>
2017-09-11 21:26:56 +1000

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -1,30 +1,51 @@
-When multiple classes share properties or methods, 
-they become candidates for inheritance — a tool developers 
-use to decrease the amount of code they need to write.
-
-With inheritance, you can create a parent class (also known as a superclass)
-with properties and methods that multiple child classes (also known as subclasses)
-share. The child classes inherit the properties and methods from their parent class. 
-
 class HospitalEmployee {
   constructor(name) {
     this._name = name;
     this._remainingVacationDays = 20;
   }
   
+  //^^creates parent class with shared keys.
+  
   get name() {
     return this._name;
   }
-
+ 
+  //^^the getter gets the value of the key from the user. 
+  
   get remainingVacationDays() {
     return this._remainingVacationDays;
   }
   
+  
+  
   takeVacationDays(daysOff) {
     this._remainingVacationDays -= daysOff;
   }
+  
+  static generatePassword() {
+    const randomNumber = Math.floor(Math.random()*10000);
+    return randomNumber;
+  }
 }
 
-//The extends keyword makes the methods of the animal class available inside the cat class.
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  } 
+  
+  get certifications() {
+    return this._certifications;
+  }
+  
+  addCertification(newCertification) {
+    this.certifications.push(newCertification);
+  }
+}
 
- //super keyword calls the constructor of the parent class
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+console.log(HospitalEmployee.generatePassword);
