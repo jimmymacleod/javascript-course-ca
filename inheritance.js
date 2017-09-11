c48c5393395d28eab890243937fd2618fa7f2d67
Jimmy MacLeod <31019678+jimmymacleod@users.noreply.github.com>
2017-09-11 17:42:06 +1000

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -5,3 +5,26 @@ use to decrease the amount of code they need to write.
 With inheritance, you can create a parent class (also known as a superclass)
 with properties and methods that multiple child classes (also known as subclasses)
 share. The child classes inherit the properties and methods from their parent class. 
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+//The extends keyword makes the methods of the animal class available inside the cat class.
+
+ //super keyword calls the constructor of the parent class
