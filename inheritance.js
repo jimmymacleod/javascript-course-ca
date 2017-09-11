fae6f40d5c1b79b3c150456ea6869645b202a1e9
Jimmy MacLeod <31019678+jimmymacleod@users.noreply.github.com>
2017-09-11 17:12:19 +1000

    Create inheritance.js

---
+++ inheritance.js
@@ -0,0 +1,7 @@
+When multiple classes share properties or methods, 
+they become candidates for inheritance — a tool developers 
+use to decrease the amount of code they need to write.
+
+With inheritance, you can create a parent class (also known as a superclass)
+with properties and methods that multiple child classes (also known as subclasses)
+share. The child classes inherit the properties and methods from their parent class. 
